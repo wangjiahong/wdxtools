4975f06b1a8090ad0de0cb83e24bb8f1bbc8db1d
dawang <dacrocodilee@gmail.com>
2016-09-12 18:44:03 +0800

    script update

--- linux-script/ubuntu-go-install.sh
+++ linux-script/ubuntu-go-install.sh
@@ -8,18 +8,23 @@ goDir=/path/to/go/folder
 
 # 下载 Go 安装包，目前版本 1.7
 # 下载地址 https://golang.org/dl/
-echo "[INFO] Download Golang binaries (1/4)"
+cd $serviceDir
+echo "[INFO] Download Golang binaries"
 wget https://storage.googleapis.com/golang/go1.7.1.linux-amd64.tar.gz
 # 删除老版本
 if [ -d "/usr/local/go" ]; then
     echo "[INFO] Remove old version"
     sudo rm -rf /usr/local/go
 fi
+echo "[INFO] Done (1/4)"
+
 # 解压
-echo "[INFO] Extract files (2/4)"
+echo "[INFO] Extract files"
 sudo tar -C /usr/local -xzf go1.7.1.linux-amd64.tar.gz
+echo "[INFO] Done (2/4)"
+
 # 更新 PATH 变量，这里支持的是 bash
-echo "[INFO] Setting PATH and GOPATH (3/4)"
+echo "[INFO] Setting PATH and GOPATH"
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
 mkdir -p goDir
 # 添加 GOPATH 变量
@@ -27,10 +32,12 @@ echo "export GOPATH="$goDir >> ~/.bashrc
 source ~/.bashrc
 go version
 echo "GOPATH="$GOPATH 
+echo "[INFO] Done (3/4)"
+
 # 清理工作，把所有的安装包保存到 software 文件夹中
-echo "[INFO] Clean up all the mess"
+echo "[INFO] Clean up all the mess "
 cd $serviceDir
 mkdir $serviceDir/software
 mv *.gz $serviceDir/software
-echo "[INFO] Done(4/4)"
+echo "[INFO] Done (4/4)"
 echo "All Done. You can now continue your work."
